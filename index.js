8002808e04693a5585ae9b892f20b0240ece35d7
mariafmedinae <mariafmedinae@gmail.com>
2022-03-16 20:29:17 -0500

    Desktop version Ready

--- index.js
+++ index.js
@@ -7,12 +7,41 @@ let totalPerson = 0;
 let selectedTip;
 const PERCENTAGE_TO_DECIMAL = 100;
 
-tipCalculator.onchange = function(event)  {
+function resetValues() {
+    bill = 0;
+    tip = 0;
+    numberPeople = 0;
     tipAmountPerson = 0;
-    totalPerson = 0;
-    
+    totalPerson = 0; 
+}
+
+function updateHTMLValues() {
+    document.getElementById("tipAmountPerson").innerHTML = tipAmountPerson.toFixed(2);
+    document.getElementById("totalPerson").innerHTML = totalPerson.toFixed(2);
+}
+
+function disableButton() {
+    const button = document.querySelector('button');    
+    if(numberPeople || tip || bill) {        
+        button.disabled = false;
+    } else {
+        button.disabled = true;
+    }
+}
+
+tipCalculator.onchange = function(event)  {  
     if(event.target.id === "bill") bill = event.target.value || 0;
-    if(event.target.id === "numberPeople") numberPeople = event.target.value || 0;
+    if(event.target.id === "numberPeople") {
+        numberPeople = event.target.value || 0;
+        if(numberPeople == 0) {
+            console.log(numberPeople);
+            document.getElementById("numberPeople").style.borderColor = "#E17052";
+            document.getElementById("errorMessage").style.display = "inline";
+        } else {
+            document.getElementById("numberPeople").style.borderColor = "transparent"; 
+            document.getElementById("errorMessage").style.display = "none";           
+        }
+    }   
     
     selectedTip = document.querySelector('input[name="tip"]:checked');
 
@@ -26,15 +55,19 @@ tipCalculator.onchange = function(event)  {
         document.getElementById('custom').value = "";
     }
 
-    if(bill && numberPeople) {
+    if(numberPeople != 0) {
         tipAmountPerson = (bill * (tip / PERCENTAGE_TO_DECIMAL)) / numberPeople;
         totalPerson = tipAmountPerson + (bill / numberPeople);
     } 
-
-    document.getElementById("tipAmountPerson").innerHTML = tipAmountPerson.toFixed(2);
-    document.getElementById("totalPerson").innerHTML = totalPerson.toFixed(2);
+    
+    disableButton();
+    
+    updateHTMLValues();
 }
 
 function resetForm() {
     document.getElementById('tipCalculator').reset();
+    resetValues();
+    updateHTMLValues();
+    disableButton();
 }
